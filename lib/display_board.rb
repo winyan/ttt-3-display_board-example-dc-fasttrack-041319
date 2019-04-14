78b35502d954e72abc6fac00de53781267cb4181
Winyan Soo Hoo <wjsoohoo@gmail.com>
2019-04-14 15:14:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# potato = "stringy thing"
+# potato = "thingy string"
+# puts potato
+def display_board
+    puts "A Tic Tac Toe Board"
+  end
